2faea19f23d33277c15d7697de2071c8eed57375
bairon <j.bayron.b@gmail.com>
2024-06-18 17:49:32 -0500

    created and implemented UpdateMeInput mutation to update user data

--- src/index.js
+++ src/index.js
@@ -1,39 +1,73 @@
-'use strict';
+'use strict'
 
 module.exports = {
   register({ strapi }) {
-    const extensionService = strapi.plugin('graphql').service('extension');
+    const extensionService = strapi.plugin('graphql').service('extension')
 
     extensionService.use(({ nexus }) => {
-      const { extendInputType, extendType } = nexus;
+      const { extendInputType, extendType } = nexus
 
       // Extend the UsersPermissionsRegisterInput input type to include phone and address
       const ExtendedUsersPermissionsRegisterInput = extendInputType({
         type: 'UsersPermissionsRegisterInput',
         definition(t) {
-          t.string('phone'); // Add phone field as String
-          t.string('address'); // Add address field
-          t.string('first_name'); // Add first_name field
-          t.string('last_name'); // Add last_name field
+          t.string('phone')
+          t.string('address')
+          t.string('first_name')
+          t.string('last_name')
         },
-      });
+      })
 
       // Extend the UsersPermissionsMe type to include the new fields
       const ExtendedUsersPermissionsMe = extendType({
         type: 'UsersPermissionsMe',
         definition(t) {
-          t.string('phone'); // Add phone field as String
-          t.string('address'); // Add address field
-          t.string('first_name'); // Add first_name field
-          t.string('last_name'); // Add last_name field
+          t.string('phone')
+          t.string('address')
+          t.string('first_name')
+          t.string('last_name')
         },
-      });
+      })
+
+      // Define the mutation to update the authenticated user's information
+      const updateMeMutation = extendType({
+        type: 'Mutation',
+        definition(t) {
+          t.field('updateMe', {
+            type: 'UsersPermissionsMe',
+            args: {
+              data: nexus.inputObjectType({
+                name: 'UpdateMeInput',
+                definition(t) {
+                  t.string('phone')
+                  t.string('address')
+                  t.string('first_name')
+                  t.string('last_name')
+                },
+              }),
+            },
+            async resolve(_root, { data }, { state: { user } }) {
+              if (!user) {
+                throw new Error('You must be authenticated to update your profile')
+              }
+
+              const updatedUser = await strapi
+                .plugin('users-permissions')
+                .service('user')
+                .edit(user.id, data)
+
+              return updatedUser
+            },
+          })
+        },
+      })
 
       return {
-        types: [ExtendedUsersPermissionsRegisterInput, ExtendedUsersPermissionsMe],
-      };
-    });
+        types: [ExtendedUsersPermissionsRegisterInput, ExtendedUsersPermissionsMe, updateMeMutation],
+      }
+    })
   },
 
-  bootstrap(/*{ strapi }*/) {},
-};
+  bootstrap(/*{ strapi }*/) {
+  },
+}
